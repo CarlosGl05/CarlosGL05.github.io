c20f142ddc6e857b09d2a423c01d797abbf5bd5e
Carlos Gloria <a00574714@tec.mx>
2026-04-21 09:31:46 -0600

    js

---
+++ api.js
@@ -0,0 +1,8 @@
+import http from 'http';
+
+const servidor = http.createServer((req, res) => {
+  console.log(“Alguien me mandó una solicitud”);
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Quiero la libertad de esculpir y cincelar mi propio rostro, de detener la hemorragia con cenizas, de crear mis propios dioses a partir de mis entrañas...\n');
+});
+
